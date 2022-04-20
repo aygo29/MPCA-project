8570a75230c0fb47fb500a5167205a44d72b54e0
ayush <ayushsash@gmail.com>
2022-04-20 19:21:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Door Lock Security System
+
+The Door Lock Security System acts as a Smart Door Lock with the help of multiple peripheral devices connected to an Arduino Uno. A special Solenoid Lock is used to keep the door secure. The door unlocks and locks only when the RFID reader detects the appropriate keycard. A buzzer paired with an ultrasonic sensor gives out an alert based on the card ID and proximity of the user.
+
+## Smart Door Lock
+
+### Parts used
+
+Arduino Uno R3
+RFID Reader
+Solenoid Lock
+Relay Module
+12V AC Adapter
+Ultrasonic Sensor
+Buzzer
+
+## Telegram Camera Bot
+
+### Parts used
+
+Arduino Uno R3
+ESP32 Cam
+9V Battery
+
+
---
+++ smart_door_lock.cpp
@@ -0,0 +1,69 @@
+/*
+
+   All the resources for this project: https://www.hackster.io/Aritro
+   Modified by Aritro Mukherjee
+
+
+*/
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+// constants won't change
+const int RELAY_PIN = A5;  // the Arduino pin, which connects to the IN pin of relay
+
+void setup()
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+
+  pinMode(RELAY_PIN, OUTPUT);
+}
+void loop()
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "77 11 EE F4") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    digitalWrite(RELAY_PIN, LOW); // uhlock the door
+    delay(2000);
+  }
+
+  else   
+  {
+    Serial.println(" Access denied");
+    digitalWrite(RELAY_PIN, HIGH);  // lock the door
+    delay(2000);
+  }
+}
